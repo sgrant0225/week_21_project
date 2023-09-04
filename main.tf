938af3da41a395487f43d4523b44008a32224245
Sharnell Grant <sharnellgrant@gmail.com>
2023-09-04 00:17:32 +0000

    created aws launch configuration

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
-#This is where the terroform code will go initial commit 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+#Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_launch_configuration" "instance_config" {
+  image_id        = "ami-051f7e7f6c2f40dc1"
+  instance_type   = "t2.micro"
+  user_data       = file("user-data.sh")
+  security_groups = [aws_security_group.instance_config.id]
+
+}
+
+
+
+
+
